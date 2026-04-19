3b4e10076adc84bfb98c543e751f6048101a8904
KRISHNAKOUNDINYA <n.koundinyakrish@gmail.com>
2026-04-19 11:46:40 +0530

    new files

--- check_jetha_loyal.sh
+++ check_jetha_loyal.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-read -p "jetha ne mud ke kis ko dekha: " bandi
-read -p "jetha ka pyar % " pyar
+# function defenition 
+function is_loyal(){
+read -p "$1 ne mud ke kis ko dekha: " bandi
+read -p "$1 ka pyar % " pyar
 if [[ $bandi == "daya babhi" ]];
 then
-   echo "jetha is loyal"
+   echo "$1 is loyal"
 elif [[ $pyar -ge 100 ]]
 then 
-    echo "jetha is loyal"
+    echo "$1 is loyal"
 else
-   echo "jetha is not loyal"
+   echo "$1 is not loyal"
 fi
+}
+
+# function call
+is_loyal "tom"
